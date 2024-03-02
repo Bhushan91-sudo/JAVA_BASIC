ebc08e743eac443615d1c79b2ebd9dc5709fb4e7
Bhushan Patle <104772925+Bhushan91-sudo@users.noreply.github.com>
2024-03-02 10:58:40 +0000

    sum

---
+++ Java_variable /sum.java
@@ -0,0 +1,19 @@
+class Initialize 
+{ 
+public static void main(String[] args) 
+{ 
+int x = 10, y = 20, z = 40; 
+int sum = x + y + z; 
+float avg = sum / 3.0f; 
+System.out.println(“SUM is : “ + sum); 
+System.out.println(“AVERAGE is : “ + avg); 
+} 
+} 
+
+/**
+ * SUM is : 70 
+AVERAGE is : 23.333333 
+RESULT:. The program to initialize 3 Variables, compute sum, average and display the result in
+JAVA has been implemented successfully. 
+
+ */
