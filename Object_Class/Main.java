6a1d5565666c33518f9f9df995381883f6f0853e
Bhushan Patle <104772925+Bhushan91-sudo@users.noreply.github.com>
2024-03-04 08:59:05 +0000

    object

---
+++ Object_Class/Main.java
@@ -0,0 +1,15 @@
+//Defining a Student cClass
+class Student {
+    //defining fields
+    int id; //field or data member or instance variable
+    String name;
+//creating main method inside the student class
+public static void main(String args[]){
+    //Creating an object or instance
+    Student s1 = new Student(); //creating an object of student
+    //Printing values of the object
+    System.out.println(s1.id);
+    System.out.println(s1.name);
+}
+}
+
